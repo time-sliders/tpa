0dcbf7df665705da33aac29aa9ef9ef41133599b
luyun <zhwwanwan@hotmail.com>
2018-09-13 17:55:19 +0800

    补充模版类型

--- src/main/java/JavaCodeGenerator.java
+++ src/main/java/JavaCodeGenerator.java
@@ -61,29 +61,52 @@ public class JavaCodeGenerator {
         }
     }
 
-
     private static void printMapper(TableConfig table, ZipOutputStream out) throws IOException {
 
         String buffer = TemplateBuilder.build(table, TemplateBuilder.MAPPER);
         printFile(buffer, table.getTableName() + ".xml", out);
 
         buffer = TemplateBuilder.build(table, TemplateBuilder.model);
-        printFile(buffer, table.getBeanName() + ".java", out);
+        printFile(buffer,  table.getBeanName() + ".java", out);
 
         buffer = TemplateBuilder.build(table, TemplateBuilder.Service);
-        printFile(buffer, table.getBeanName() + "Service.java", out);
+        printFile(buffer,  table.getBeanName() + "Service.java", out);
 
         buffer = TemplateBuilder.build(table, TemplateBuilder.ServiceImpl);
-        printFile(buffer, table.getBeanName() + "ServiceImpl.java", out);
+        printFile(buffer,  table.getBeanName() + "ServiceImpl.java", out);
 
         buffer = TemplateBuilder.build(table, TemplateBuilder.DAO);
-        printFile(buffer, table.getBeanName() + "DAO.java", out);
+        printFile(buffer,  table.getBeanName() + "DAO.java", out);
 
         buffer = TemplateBuilder.build(table, TemplateBuilder.DAOImpl);
-        printFile(buffer, table.getBeanName() + "DAOImpl.java", out);
+        printFile(buffer,  table.getBeanName() + "DAOImpl.java", out);
 
         buffer = TemplateBuilder.build(table, TemplateBuilder.DOConverter);
-        printFile(buffer, table.getBeanName() + "DOConverter.java", out);
+        printFile(buffer,  table.getBeanName() + "DOConverter.java", out);
+
+        buffer = TemplateBuilder.build(table, TemplateBuilder.Query);
+        printFile(buffer,  table.getBeanName() + "Query.java", out);
+
+        buffer = TemplateBuilder.build(table, TemplateBuilder.DO);
+        printFile(buffer,  table.getBeanName() + "DO.java", out);
+
+        buffer = TemplateBuilder.build(table, TemplateBuilder.dtoModel);
+        printFile(buffer,  table.getBeanName() + "DTO.java", out);
+
+        buffer = TemplateBuilder.build(table, TemplateBuilder.DTOConverter);
+        printFile(buffer,  table.getBeanName() + "DTOConverter.java", out);
+
+        buffer = TemplateBuilder.build(table, TemplateBuilder.ManageFacade);
+        printFile(buffer,  table.getBeanName() + "ManageFacade.java", out);
+
+        buffer = TemplateBuilder.build(table, TemplateBuilder.ManageFacadeImpl);
+        printFile(buffer,  table.getBeanName() + "ManageFacadeImpl.java", out);
+
+        buffer = TemplateBuilder.build(table, TemplateBuilder.QueryFacade);
+        printFile(buffer,  table.getBeanName() + "QueryFacade.java", out);
+
+        buffer = TemplateBuilder.build(table, TemplateBuilder.QueryFacadeImpl);
+        printFile(buffer,  table.getBeanName() + "QueryFacadeImpl.java", out);
 
         out.flush();
     }
--- src/main/java/dependency/TemplateBuilder.java
+++ src/main/java/dependency/TemplateBuilder.java
@@ -16,6 +16,15 @@ public class TemplateBuilder {
     public final static String ServiceImpl = "ServiceImpl.vm";
     public final static String DOConverter = "DOConverter.vm";
 
+    public final static String Query = "Query.vm";
+    public final static String DO = "DO.vm";
+    public final static String dtoModel = "modelDTO.vm";
+    public final static String DTOConverter = "DTOConverter.vm";
+    public final static String ManageFacade = "ManageFacade.vm";
+    public final static String ManageFacadeImpl = "ManageFacadeImpl.vm";
+    public final static String QueryFacade = "QueryFacade.vm";
+    public final static String QueryFacadeImpl = "QueryFacadeImpl.vm";
+
     public static String build(TableConfig table, String templateName) {
         VelocityContext velocityContext = new VelocityContext();
         ByteArrayOutputStream temp = new ByteArrayOutputStream();
